d7e2571ccde1265256e76e4531e7e95293ad517a
Junyoung Kim <jkim5929@gmail.com>
2023-01-09 15:24:29 -0500

    Added spacing

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
 
-Console.WriteLine("Enter Length: ");
+Console.WriteLine("\nEnter Length: ");
 double roomLength = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter Width: ");
+Console.WriteLine("\nEnter Width: ");
 double roomWidth = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter Height: ");
+Console.WriteLine("\nEnter Height: ");
 double roomHeight = double.Parse(Console.ReadLine());
 
 double Area = roomLength * roomWidth;
@@ -13,22 +13,22 @@ double Perimeter = (roomLength * 2) + (roomWidth * 2);
 double Volume = roomWidth * roomHeight * roomLength;
 double surfaceArea = (2 * (roomLength * roomHeight)) + (2 * (roomWidth * roomHeight)) + (2 * (roomLength * roomWidth));
 
-Console.WriteLine("Area: "+ Area);
-Console.WriteLine("Perimeter: " + Perimeter);
-Console.WriteLine("Volume: " + Volume);
-Console.WriteLine("Surface Area:" +surfaceArea);
+Console.WriteLine("\nArea: "+ Area);
+Console.WriteLine("\nPerimeter: " + Perimeter);
+Console.WriteLine("\nVolume: " + Volume);
+Console.WriteLine("\nSurface Area:" +surfaceArea);
 
 if (Perimeter <= 250)
 {
-    Console.WriteLine("This is a small sized room");
+    Console.WriteLine("\nThis is a small sized room");
 } 
 else if (Perimeter > 250 && Perimeter < 650) 
 {
-    Console.WriteLine("This is a medium sized room");
+    Console.WriteLine("\nThis is a medium sized room");
 }
 else if (Perimeter >= 650)
 {
-    Console.WriteLine("This is a large sized room");
+    Console.WriteLine("\nThis is a large sized room");
 }
 
-Console.WriteLine("Thank you for using the Room Detail Generator!");
+Console.WriteLine("\nThank you for using the Room Detail Generator!");
